2356cabe5bee550f0e3c77ce5d1e76127637a0ea
alexanderrusinov-png <alexander.rusinov@gmail.com>
2026-01-12 18:56:23 +0200

    add default demographics

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 from src.layout import setup_layout
 # from src.callbacks import register_callbacks
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEFAULT_DEMOGRAPHICS_PATH = os.path.join(
+    BASE_DIR, "data", "demographics", "default_demographics.json"
+)
+
+DEFAULT_ANALYSIS_PATH = os.path.join(
+    BASE_DIR, "data", "analysis", "default_analysis.json"
+)
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 app.layout = setup_layout
